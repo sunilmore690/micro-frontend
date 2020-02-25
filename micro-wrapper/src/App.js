b861c07f5918ca550ab64698023cf5d1c8b391a5
Sunil More <sunilmore690@gmail.com>
2020-02-25 23:49:17 +0530

    first commit

---
+++ micro-wrapper/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+       <h1>Parent Section</h1>
+       
+      <div className="App">
+        <div className="app">
+          <app-1></app-1>
+        </div>
+        <div className="app">
+          <app-2></app-2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
